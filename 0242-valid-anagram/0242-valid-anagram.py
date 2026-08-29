0c0b8c5426ef578954580e43ee47e8d9ac1c17e0
Aditya Sharma <165385979+AdityaSharma-dev-codes@users.noreply.github.com>
2026-08-29 16:33:19 +0530

    0242-VALID-ANAGRAM Stats: Time: 17 ms (21.47%), Space: 19.5 MB (46.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,22 +2,16 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         check = dict()
         for i in s:
-            if i in check:
-                check[i] += 1
-            else:
-                check[i] = 1
+            check[i] = check.get(i, 0) + 1
         
         for i in t:
-            if i not in check:
+            if i not in check or check[i] == 0:
                 return False
-            if i in check:
-                if check[i] == 0:
-                    return False
-                else:
-                    check[i] -= 1
+            else:
+                check[i] -= 1
         
         for i in s:
             if check[i] != 0:
                 return False
-                
+
         return True
